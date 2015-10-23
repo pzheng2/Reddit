310fc18a01d1061bd1f985ebab8fb9a309d1e4e6
App Academy Student <appacademy@Apps-Mac-mini-24.local>
2015-10-23 11:25:28 -0400

    Create user model/validations.

---
+++ RedditClone/app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :username, :session_token, uniqueness: true
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+  
+  def self.generate_session_token
+    SecureRandom.base64(16)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+end
